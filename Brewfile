df780d4d65248ac0700ac5677332633397fd4095
나윤호 <lucky7_nyh@naver.com>
2018-06-25 10:42:06 +0900

    [fix] brew 적용 중

---
+++ Brewfile
@@ -0,0 +1,42 @@
+#tap
+tap "homebrew/core"
+tap "homebrew/bundle"
+tap "homebrew/services"
+tap "homebrew/dupes"
+tap "homebrew/cask"
+tap "homebrew/cask-versions"
+tap "homebrew/cask-fonts"
+
+#vcs
+brew "git"
+brew "subversion"
+
+#node
+brew "nvm"
+
+#editor
+cask 'visual-studio-code'
+
+#ide
+cask 'intellij-idea'
+
+#font
+cask "homebrew/cask-fonts/font-d2coding"
+
+#util
+brew "zsh"
+cask "zazu"
+cask 'iterm2'
+cask 'google-chrome'
+cask 'firefox'
+
+#appstore util
+brew "mas"
+mas "Irvue", id: 1039633667
+mas "Monosnap", id: 540348655
+mas "Keynote", id: 409183694
+mas "Numbers", id: 409203825
+mas "Pages", id: 409201541
+mas "Trello", id: 1278508951
+mas "Wunderlist", id: 410628904
+mas "KakaoTalk", id: 869223134
--- brew.sh
+++ brew.sh
@@ -3,49 +3,13 @@ if ! [[ -x "$(command -v brew)" ]]; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
+dir=${pwd}
+
 # Update, upgrade
 brew update
 brew upgrade
 
-#tap
-tap "homebrew/core"
-tap "homebrew/bundle"
-tap "homebrew/services"
-tap "homebrew/dupes"
-tap "homebrew/cask"
-tap "homebrew/cask-versions"
-tap "homebrew/cask-fonts"
+brew bundle --file=$dir/Brewfile
 
-#vcs
-brew "git"
-brew "subversion"
-
-#node
-brew "nvm"
-
-#editor
-cask 'visual-studio-code'
-
-#ide
-cask 'intellij-idea'
-
-#font
-cask "homebrew/cask-fonts/font-d2coding"
-
-#util
-brew "zsh"
-cask "zazu"
-cask 'iterm2'
-cask 'google-chrome'
-cask 'firefox'
-
-#appstore util
-brew "mas"
-mas "Irvue", id: 1039633667
-mas "Monosnap", id: 540348655
-mas "Keynote", id: 409183694
-mas "Numbers", id: 409203825
-mas "Pages", id: 409201541
-mas "Trello", id: 1278508951
-mas "Wunderlist", id: 410628904
-mas "KakaoTalk", id: 869223134
+brew cleanup
+brew cask cleanup
