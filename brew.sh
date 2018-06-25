af21353b57db0c0e1e923f24fa6ace1c7dcbc9e2
나윤호 <lucky7_nyh@naver.com>
2018-06-25 10:52:30 +0900

    [fix] brew 적용 중

--- brew.sh
+++ brew.sh
@@ -3,7 +3,7 @@ if ! [[ -x "$(command -v brew)" ]]; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-DOTFILES_DIR := $(shell pwd)
+DOTFILES_DIR=$(pwd)
 
 # Update, upgrade
 brew update
