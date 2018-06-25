f065c3ff9185b93a9a44eee20efb5da01a65f118
나윤호 <lucky7_nyh@naver.com>
2018-06-25 10:50:23 +0900

    [fix] brew 적용 중

--- brew.sh
+++ brew.sh
@@ -3,13 +3,13 @@ if ! [[ -x "$(command -v brew)" ]]; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-dir=${pwd}
+DOTFILES_DIR := $(shell pwd)
 
 # Update, upgrade
 brew update
 brew upgrade
 
-brew bundle --file=${dir}/Brewfile
+brew bundle --file=${DOTFILES_DIR}/Brewfile
 
 brew cleanup
 brew cask cleanup
