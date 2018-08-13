409b03f78a5a7177d146d441f7108add378219e0
nyh <lucky72nyh@gmail.com>
2018-08-13 21:57:14 +0900

    [fix] nvm 설정

--- npm.sh
+++ npm.sh
@@ -1,5 +1,9 @@
 # install nvm (script from https://github.com/creationix/nvm)
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+
 # install node
 nvm install lts/carbon
 nvm install stable
