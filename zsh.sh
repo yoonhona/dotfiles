8d700e54adbb217e7b03c5261f1397328979cfe4
nyh <nyh@mrblue.com>
2020-05-25 09:33:08 +0900

    fix: zsh 테마 설치 권한

--- zsh.sh
+++ zsh.sh
@@ -6,8 +6,8 @@ chsh -s `which zsh`
 
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-rm -rf ~/.zshrc
+sudo rm -rf ~/.zshrc
 ln -s ~/dotfiles/.config/.zshrc ~/
 
-git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/
+sudo git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/
 
