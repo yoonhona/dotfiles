1db58eb70601f4845ba2b9b10acd4b3085d087f0
yoonhona <lucky72nyh@gmail.com>
2020-05-28 01:02:45 +0900

    feat: 선택된 영역 구글 번역 페이지로 기능 추가

--- .config/.hammerspoon/init.lua
+++ .config/.hammerspoon/init.lua
@@ -1,9 +1,10 @@
-mash = {"⌘", "⌥", "⌃"}
+mash = {"⌘", "⌥", "⌃"} -- ⇧
 
 require ("hotkey")
 require("window")
 require("apps")
 require("inputSource")
+require("translate")
 
 hs.hotkey.bind(mash, "r", function() hs.reload(); end)
 
