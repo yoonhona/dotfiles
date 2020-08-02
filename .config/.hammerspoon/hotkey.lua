c76d2d7ead481ff9d7a059808587c92a858fde8d
yoonhona <lucky72nyh@gmail.com>
2020-08-02 22:59:13 +0900

    fix: 마우스 우클릭 추가

--- .config/.hammerspoon/hotkey.lua
+++ .config/.hammerspoon/hotkey.lua
@@ -8,3 +8,10 @@ end)
 hs.hotkey.bind(mash, "L", function()
     hs.caffeinate.lockScreen()
 end)
+
+-- 현재 마우스 커서의 우클릭
+hs.hotkey.bind(mash, "r", function()
+    local pos=nil
+    pos=hs.mouse.getAbsolutePosition()
+    hs.eventtap.rightClick(pos, 0)
+end)
--- .config/.hammerspoon/init.lua
+++ .config/.hammerspoon/init.lua
@@ -5,6 +5,3 @@ require("window")
 require("apps")
 require("inputSource")
 require("translate")
-
-hs.hotkey.bind(mash, "r", function() hs.reload(); end)
-
