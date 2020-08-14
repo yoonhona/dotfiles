cea2c435714155d421de7d89b22a330cc567b0f7
nyh <nyh@mrblue.com>
2020-08-14 13:47:47 +0900

    fix: 사용하지 않는 파일 제거

--- scripts/util.sh
+++ scripts/util.sh
@@ -4,10 +4,6 @@ if ! [[ -x "$(command -v jabba)" ]]; then
     curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash && . ~/.jabba/jabba.sh
 fi
 
-# zazu
-rm -rf ~/.zazurc.json
-ln -s ~/dotfiles/.config/.zazurc.json ~/
-
 # hammerspoon
 rm -rf ~/.hammerspoon
 ln -s ~/dotfiles/.config/.hammerspoon ~/
